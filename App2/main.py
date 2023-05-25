eff79c7191b505133e2df313c4fdb24dd35e007e
Gaweł Banasik <83306952+Gawello@users.noreply.github.com>
2023-05-25 23:00:47 +0200

    PDF generator is ready!

--- App2/main.py
+++ App2/main.py
@@ -1,3 +1,4 @@
+import webbrowser
 from fpdf import FPDF
 
 
@@ -38,15 +39,20 @@ class PdfReport:
         pdf = FPDF(orientation='P', unit='pt', format='A4')
         pdf.add_page()
 
+        # Add icon
+        pdf.image(name="house.png", w=30, h=30)
+
         # Insert title
         pdf.set_font(family='Times', size=24, style='B')
-        pdf.cell(w=0, h=80, txt="Flatmates Bill", border=1, align="C", ln=1)
+        pdf.cell(w=0, h=80, txt="Flatmates Bill", border=0, align="C", ln=1)
 
         # Insert Period label and value
-        pdf.cell(w=100, h=40, txt="Period:", border=1)
-        pdf.cell(w=150, h=40, txt=bill.period, border=1, ln=1)
+        pdf.set_font(family="Times", size=14, style='B')
+        pdf.cell(w=100, h=40, txt="Period:", border=0)
+        pdf.cell(w=150, h=40, txt=bill.period, border=0, ln=1)
 
         # Insert name and due amount to pay of second flatmate
+        pdf.set_font(family="Times", size=12)
         pdf.cell(w=100, h=40, txt=flatmate1.name, border=1)
         pdf.cell(w=150, h=40, txt=f"{flatmate1.pays(bill, flatmate2=flatmate2)} PLN", border=1, ln=1)
 
@@ -55,6 +61,12 @@ class PdfReport:
         pdf.cell(w=150, h=40, txt=f"{flatmate2.pays(bill, flatmate2=flatmate1)} PLN", border=1, ln=1)
 
         pdf.output(f"Bill_{bill.period}.pdf")
+
+        # Open PDF (Linux/Mac version)
+        # webbrowser.open('file://' + os.path.realpath(self.filename))
+
+        # Open PDF in Windows
+        # webbrowser.open(self.file_name)
 
 
 the_bill = Bill(amount=120, period="March 2023")
