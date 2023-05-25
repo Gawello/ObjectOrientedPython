c75bbac8fc02322a0f2a820378ef31ca131899a8
Gaweł Banasik <83306952+Gawello@users.noreply.github.com>
2023-05-25 21:39:24 +0200

    First attempt for PDF generation

--- App2/main.py
+++ App2/main.py
@@ -1,3 +1,6 @@
+from fpdf import FPDF
+
+
 class Bill:
     """
     Object that contains data about a bill, such as total amount and period of the bill.
@@ -20,7 +23,7 @@ class Flatmate:
     def pays(self, bill, flatmate2):
         weight = self.days_in_house / (self.days_in_house + flatmate2.days_in_house)
         to_pay = bill.amount * weight
-        return to_pay
+        return round(to_pay, 2)
 
 
 class PdfReport:
@@ -32,7 +35,26 @@ class PdfReport:
         self.file_name = file_name
 
     def generate(self, flatmate1, flatmate2, bill):
-        pass
+        pdf = FPDF(orientation='P', unit='pt', format='A4')
+        pdf.add_page()
+
+        # Insert title
+        pdf.set_font(family='Times', size=24, style='B')
+        pdf.cell(w=0, h=80, txt="Flatmates Bill", border=1, align="C", ln=1)
+
+        # Insert Period label and value
+        pdf.cell(w=100, h=40, txt="Period:", border=1)
+        pdf.cell(w=150, h=40, txt=bill.period, border=1, ln=1)
+
+        # Insert Flatmate1 and their bill to pay
+        pdf.cell(w=100, h=40, txt=flatmate1.name, border=1)
+        pdf.cell(w=150, h=40, txt=f"{flatmate1.pays(bill, flatmate2=flatmate2)} PLN", border=1, ln=1)
+
+        # Insert Flatmate2 and their bill to pay
+        pdf.cell(w=100, h=40, txt=flatmate2.name, border=1)
+        pdf.cell(w=150, h=40, txt=f"{flatmate2.pays(bill, flatmate2=flatmate1)} PLN", border=1, ln=1)
+
+        pdf.output(f"Bill_{bill.period}.pdf")
 
 
 the_bill = Bill(amount=120, period="March 2023")
@@ -41,3 +63,6 @@ marry = Flatmate(name="Marry", days_in_house=25)
 
 print("John pays: ", john.pays(bill=the_bill, flatmate2=marry))
 print("Marry pays: ", marry.pays(the_bill, john))
+
+pdf_report = PdfReport(file_name="Report1.pdf")
+pdf_report.generate(flatmate1=john, flatmate2=marry, bill=the_bill)
